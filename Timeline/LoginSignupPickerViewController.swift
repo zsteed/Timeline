72b98d5b036f28bdce7d3e73b84e75aa24034391
Zach Steed <zsteed21@gmail.com>
2015-11-03 14:57:53 -0700

    finished view hierarchy set up

---
+++ Timeline/LoginSignupPickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginSignupPickerViewController.swift
+//  Timeline
+//
+//  Created by Zach Steed on 11/3/15.
+//  Copyright © 2015 NobleZ. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignupPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
